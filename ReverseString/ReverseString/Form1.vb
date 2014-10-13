66050e50f17e626eeee2c165a2201cffb00f8cd5
idsc3101 <idsc3101@gmail.com>
2014-10-13 15:15:41 -0500

    work day 8

--- ReverseString/ReverseString/Form1.vb
+++ ReverseString/ReverseString/Form1.vb
@@ -6,7 +6,16 @@ Public Class Form1
         Dim st As String = txtInput.Text
 
         ' Please write your code here!
+        Dim result As String
+        For i As Integer = st.Length - 1 To 0 Step -1
+            result = result & st.Chars(i)
 
-        txtOutput.Text = "Not implemented yet!"
+
+        Next
+        txtOutput.Text = result
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
     End Sub
 End Class
